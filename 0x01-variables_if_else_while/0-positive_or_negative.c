ca83694105b70f6f15365c8758ca6d727a1bf175
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-06 16:14:19 -0700

    {push it to git}

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,20 +9,20 @@
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (a>0){
-	  printf("%d is positive\n", n);
-	}
-	else if (a=0)
-	  {
-	    printf("%d is zero\n", n);
-	  }
-	else (a<0)
-	       {
-		 printf("%d is negative\n", n);
-	       }
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (a > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (a = 0)
+{
+printf("%d is zero\n", n);
+}
+else (a < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0x01. C - Variables, if, else, while | 0-positive_or_negative.c | 1
 0x01. C - Variables, if, else, while | 0-positive_or_negative.c | 1.1 README file is added
+push it to git
