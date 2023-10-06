c145c8e896ab4f30c4448ca27b8ab239c3d23617
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-06 16:05:18 -0700

    {0x01. C - Variables, if, else, while | 0-positive_or_negative.c | 1}

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - The main function, which serves as the entry point of the program.
+ *
+ * This function prints "Hello, World!" to the console and returns 0.
+ *
+ * Return: 0 on successful execution.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (a>0){
+	  printf("%d is positive\n", n);
+	}
+	else if (a=0)
+	  {
+	    printf("%d is zero\n", n);
+	  }
+	else (a<0)
+	       {
+		 printf("%d is negative\n", n);
+	       }
+	return (0);
+}
