1b7871950285e5633a51a33e303cff87bf8e61d5
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-05 18:53:35 -0700

    {0x00. C - Hello, World | 6-size.c | 1}

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The main function, which serves as the entry point of the program.
+ *
+ * This function prints "Hello, World!" to the console and returns 0.
+ *
+ * Return: 0 on successful execution.
+ */
+int main(void)
+{
+printf("Size of a char:", sizeof(char))
+printf("Size of a int:", sizeof(int))
+printf("Size of a long int:", sizeof(long int))
+printf("Size of a long long int:", sizeof(long long int))
+printf("Size of a float:", sizeof(float))
+return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -12,3 +12,4 @@ Making 0x00-hello_world Dir
 0x00. C - Hello, World | 4-puts.c | 6
 0x00. C - Hello, World | 4-puts.c | 7
 0x00. C - Hello, World | 5-printf.c | 1
+0x00. C - Hello, World | 5-printf.c | 2: adding new line
