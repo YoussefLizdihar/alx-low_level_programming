1f9cf98494a4d3135b263e28a1bd9b2410a274b4
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-05 18:33:16 -0700

    {0x00. C - Hello, World | 4-puts.c | 6}

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 /**
  * main - Prints "Hello, World!" to the console.
  *
- * This is the main function, which serves as the entry point of the program.
- * It prints the "Hello, World!" message to the console using the `puts` function.
+ * This is the main function.
  *
- * return 0 on successful execution.
+ * return: always 0 on successful execution.
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
