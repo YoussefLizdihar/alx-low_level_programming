de8eef05992c97b58e864f92649f4f8968a1ad64
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-06 17:01:36 -0700

    {0x01. C - Variables, if, else, while | 1-last_digit.c | 3 | I add n}

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ printf("Last digit of %d is %d ", n, n % 10);
 n = n % 10;
 if (n > 5)
 {
-printf("and is greater than 5");
+printf("and is greater than 5\n");
 }
 else if (n == 0)
 {
-printf("and is 0");
+printf("and is 0\n");
 }
 else if (n < 6 && n != 0)
 {
-printf("and is less than 6 and not 0");
+printf("and is less than 6 and not 0\n");
 }
 return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -7,3 +7,4 @@ push it to git
 push it to github
 0x01. C - Variables, if, else, while | 1-last_digit.c | 1
 0x01. C - Variables, if, else, while | 1-last_digit.c | 2
+0x01. C - Variables, if, else, while | 1-last_digit.c | 3 | I add n
