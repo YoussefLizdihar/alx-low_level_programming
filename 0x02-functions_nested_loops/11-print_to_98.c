d61afb523e750c58587da0d39c9e0c09cedb2cdf
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-11 13:57:43 -0700

    {h}

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 while (n < 98)
 {
 printf("%d", n);
-if (n < 98)
+if (n < 97)
 {
 printf(", ");
 }
@@ -19,7 +19,7 @@ n++;
 while (n > 98)
 {
 printf("%d", n);
-if (n > 98)
+if (n > 99)
 {
 printf(", ");
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -61,3 +61,4 @@ lclc
 djdj
 k
 jh
+h
