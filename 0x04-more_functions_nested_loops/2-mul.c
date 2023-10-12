65d0cc82e9583699879b9375e94c72ef4b92c6d3
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-12 13:39:13 -0700

    {create 2-mul.c}

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul - a function that multiplies two integers.
+ * Return: 1 if success.
+ * @a: this is an int
+ * @b: an int
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+
+return (result);
+
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 create: 0x04-more_functions_nested_loops dir, 1-isdigit.c file
 create 0-isupper.c
+create 2-mul.c
