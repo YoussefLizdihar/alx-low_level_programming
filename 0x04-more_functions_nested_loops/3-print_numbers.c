198ecc6cd4004be3dd49e3456387fd9b89af1d3e
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-12 14:07:23 -0700

    {update 3-print_numbers.c}

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,14 @@
  * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line.
  * Return: if success.
  */
+
+
 void print_numbers(void)
 {
-int i;
 
+int i;
 i = 0;
+
 while (i < 10)
 {
 _putchar('0' + i);
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,4 @@ create 2-mul.c
 create 3-print_numbers.c
 update 3-print_numbers.c
 update 3-print_numbers.c
+update 3-print_numbers.c
