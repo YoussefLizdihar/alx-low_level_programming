dd454dbe3a7e408bc9733881654fd8bbdbdd030c
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-12 14:03:18 -0700

    {update 3-print_numbers.c}

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9.
+ * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line.
  * Return: if success.
  */
 void print_numbers(void)
 {
-int i = 0;
-while (i > 9)
+int i;
+
+i = 0;
+while (i < 10)
 {
-_putchar(i + '0');
+_putchar('0' + i);
 i++;
 }
-_putchar('\n');
 
-return (0);
+return;
 
 }
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@ create 0-isupper.c
 create 2-mul.c
 create 3-print_numbers.c
 update 3-print_numbers.c
+update 3-print_numbers.c
