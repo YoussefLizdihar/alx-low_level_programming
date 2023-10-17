d3dc68edf50da98689ea7221cd329ec627ad6ea8
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-16 17:38:03 -0700

    {create the file n 1}

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * swap_int - a function
+ * Return: 1 if c uppercasetrue, 0 otherwise.
+ * @a: this is a var
+ * @b: this is a var
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+return;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 k
 create the file N 0, README file, main.h header file
+update the file n 0
