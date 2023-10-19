a9171cc41acf07e8f4735145f3c92bd52f04da4d
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-19 15:48:41 -0700

    {update  2-strncpy.c}

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,13 +6,24 @@
  * @src: this is the char
  * @n: this is the int
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
+int j = 0;
 while (i < n)
 {
 dest[i] = src[i];
 i++;
+}
+while (src[j] != '\0')
+{
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
 }
 return (dest);
 }
