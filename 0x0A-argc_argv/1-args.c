01996a88e98a6001da65fa4f52b408d73556363c
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-27 18:36:40 -0700

    {create 1-args.c}

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a function
+ * Return: 1 if true, 0 otherwise.
+ * @argc: this is a var
+ */
+int main(int argc)
+{
+printf("%d\n", argc);
+return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,3 @@
 README LOG FILE
 create 0-whatsmyname.c
+update 0-whatsmyname.c
