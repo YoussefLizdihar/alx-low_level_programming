dd359c92b4fd26e752592d0aea2717ad40468e71
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-27 17:48:15 -0700

    {update}

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -9,21 +9,4 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
-int j = 0;
-while (i < n)
-{
-dest[i] = src[i];
-i++;
-}
-while (src[j] != '\0')
-{
-j++;
-}
-while (j < n)
-{
-dest[j] = '\0';
-j++;
-}
-return (dest);
 }
--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,3 +1,4 @@
 
 create libmy.a, Readme files
 create create_static_lib.sh
+update create_static_lib.sh
