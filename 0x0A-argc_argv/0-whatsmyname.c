2193a4393914fa75032bdc7f2b8c443b37a2b923
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-10-27 18:29:17 -0700

    {create 0-whatsmyname.c}

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a function
+ * Return: 1 if true, 0 otherwise.
+ * @argv: this is the char
+ */
+
+int main(char *argv[])
+{
+printf("%s \n", argv[0]);
+return (0);
+}
