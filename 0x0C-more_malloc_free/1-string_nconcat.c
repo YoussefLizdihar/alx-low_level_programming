9a423368b8ab085aa1e635269c36277a21ab6c2a
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-11-03 16:30:26 -0700

    {uo}

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *ptr;
-unsigned int a = 0, b = 0, i, j;
+unsigned int a = 0, b = 0, i = 0, j = 0;
 if (s1 == NULL)
 {
 s1 = "";
@@ -20,8 +20,10 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-for (i = 0; s1[i] != '\0'; i++)
-for (j = 0; s2[j] != '\0'; j++)
+while (s1[i] != '\0')
+i++;
+while (s2[j] != '\0')
+j++;
 ptr = malloc(i + n + 1);
 if (ptr == NULL)
 {
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -8,3 +8,4 @@ update 0 last but not least
 create 1
 update 1
 up
+update 1
