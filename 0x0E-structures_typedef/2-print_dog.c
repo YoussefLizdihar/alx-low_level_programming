424f33603e403a68d4c1f0d97e6e5082cbb71372
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-11-07 17:17:14 -0800

    {2}

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,4 @@ printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
-else if (d == NULL)
-{
-printf(" ");
-}
 }
