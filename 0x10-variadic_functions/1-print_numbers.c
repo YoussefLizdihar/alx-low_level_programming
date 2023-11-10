b08b05d8f9cbd7250b81df734e4394e20f130395
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-11-09 18:39:59 -0800

    {update}

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i = 0;
 int sum = 0;
-
 va_list args;
+if (n == 0)
+{
+printf("\n");
+return;
+}
 va_start(args, n);
 while (i < n)
 {
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,2 +1,3 @@
 LOGs File
 create : || Header, README, 0|| Files
+///////////////////////// Create  1-print_numbers.c
