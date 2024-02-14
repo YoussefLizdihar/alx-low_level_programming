38d2d65d980f62a2d47d686f2f6bb618edf0ef58
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-02-14 15:28:59 -0800

    {update 5}

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 unsigned long int xornum = n ^ m;
 unsigned int i = 0;
 
-while (xor_result > 0)
+while (xornum > 0)
 {
 if (xornum & 1)
 i++;
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -6,3 +6,4 @@ update 1
 create 2
 create 3
 create 4
+create 5
